9ba53a88a558df1a9b8b471b474694b0935ff9bb
Wendy Teo <wendyteo.wy@gmail.com>
2022-07-21 08:45:02 +1000

    Created Header and added styling

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import style from './Header.module.css';
+
+function Header () {
+    const header = (
+        <header>
+            <div className={style.resumeHeader}>
+                <div className={style.profileName}>
+                    <p>Wendy Teo</p>
+                </div>
+                <div className={style.contactDetails}>
+                    <p>wendy.teo@gmail.com</p>
+                    <p>(+61) 458 369 510</p>
+                    <p>github.com/wteo</p>
+                </div>
+            </div>
+
+            <nav>
+                <p>About Me</p>
+                <p>Education</p>
+                <p>Projects</p>
+                <p>Experience</p>
+                <p>Skills</p>
+            </nav>
+        </header>
+    );
+
+    return header;
+}
+
+export default Header;
