f49ec31377d3d17a0ac1b3c011e701d9974ee14f
Wendy Teo <wendyteo.wy@gmail.com>
2022-07-21 18:12:23 +1000

    Added profile picture

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React from 'react';
 
 import style from './Header.module.css';
+
+import picture from '../images/profile.jpg';
+
 
 function Header () {
     const header = (
         <header>
             <div className={style.resumeHeader}>
                 <div className={style.profileName}>
+                    <div>
+                        <img className={style.profilePicture} src={picture} alt="My Happy Face" />
+                    </div>
                     <p>Wendy Teo</p>
                 </div>
                 <div className={style.contactDetails}>
