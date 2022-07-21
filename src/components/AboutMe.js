83637ad4968a5d6c250beb454324a6f4a574a530
Wendy Teo <wendyteo.wy@gmail.com>
2022-07-22 09:27:47 +1000

    Added AboutMe component

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 import NavTitleStyle from '../UI/Nav-Title-style';
 
+import styles from './AboutMe.module.css';
+
 function AboutMe() {
     return (
-        <div>
-            <NavTitleStyle navColor='main'>
+        <div className={styles.aboutMeContainer}>
+            <div>
+                <NavTitleStyle navColor='main'>
                 About Me
-            </NavTitleStyle>
+                </NavTitleStyle>
+            </div>
+            <p className={styles.aboutMeSubTitle}>Self-Taught Developer</p>
             <p>I am a full-time professional, who has been studying web development slightly over a year now.</p>
             <p>After 9 years in Finance, I decided to pursue my interest in web design.</p>
         </div>
