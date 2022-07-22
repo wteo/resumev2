bdb0b82befd514378a9cec7b08ac34c27d265444
Wendy Teo <wendyteo.wy@gmail.com>
2022-07-23 09:15:04 +1000

    Added styling to AboutMe components

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,20 +3,31 @@ import React from 'react';
 import NavTitleStyle from '../../UI/Nav-Title-style';
 
 import styles from './AboutMe.module.css';
+import profilePicture from '../../images/profile2.jpg';
 
 function AboutMe() {
     return (
-        <div className={styles.aboutMeContainer}>
+        <div className={styles.aboutMeOuterContainer}>
             <div>
                 <NavTitleStyle navColor='main'>
-                About Me
+                    About Me
                 </NavTitleStyle>
             </div>
-            <p className={styles.aboutMeSubTitle}>Self-Taught Developer</p>
-            <p>I am a full-time professional, who has been studying web development slightly over a year now.</p>
-            <p>After 9 years in Finance, I decided to pursue my interest in web design.</p>
+            <div className={styles.aboutMeInnerContainer}>
+                <div className={styles.aboutMeSummary}>
+                    <p className={styles.aboutMeSubTitle}>Self-Taught Developer</p>
+                    <p>I am a full-time professional, who has been studying web development slightly over a year now.</p>
+                    <p>After 9 years in Finance, I decided to pursue my interest in web design.</p>
+                    <br/>
+                    <p className={styles.aboutMeSubTitle}>General</p>
+                    <p>I was born in Malaysia. Hence, I can speak 4 different languages: English (my most used language), Malay, Mandarin and Hokkien.</p>
+                    <p>For leisure, I enjoy reading cycling and reading books.</p>
+                </div>
+                <div className={styles.aboutMeImageContainer}>
+                    <img className={styles.profileImage} src={profilePicture} alt='myself'/>
+                </div>
+            </div>
         </div>
-
     );
 }
 
