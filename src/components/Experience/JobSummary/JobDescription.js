24d3964623f4dbf483064f3f71b041138e2794ba
Wendy Teo <wendyteo.wy@gmail.com>
2022-07-22 20:23:36 +1000

    Changed dot points

--- src/components/Experience/JobSummary/JobDescription.js
+++ src/components/Experience/JobSummary/JobDescription.js
@@ -6,17 +6,17 @@ function JobDescription() {
     return (
         <div className={styles.jobDescriptionStyle}>
             <div className={styles.descriptionContainer}>
-                <p className={styles.dotPoint}>&#9830;</p>
+                <p className={styles.dotPoint}>*</p>
                 <p className={styles.descriptionPar}>Involves in key projects to automate parts of Accounts Receivables (AR) procedure, testing system and providing feedback.</p>
                 <p className={styles.whiteSpaceAfterDescription}></p>
             </div>
             <div className={styles.descriptionContainer}>
-                <p className={styles.dotPoint}>&#9830;</p>
+                <p className={styles.dotPoint}>*</p>
                 <p className={styles.descriptionPar}>Maintains day-to-day communication with clients via email/phone concerning aged/outstanding debts, liaising with high-profile personas, including CFOs, Company Secretaries, Senior Accountants, etc.</p>
                 <p className={styles.whiteSpaceAfterDescription}></p>
             </div>
             <div className={styles.descriptionContainer}>
-                <p className={styles.dotPoint}>&#9830;</p>
+                <p className={styles.dotPoint}>*</p>
                 <p className={styles.descriptionPar}>Where necessary, contacts clients for payment and arranges/negotiates payment schedules.</p>
                 <p className={styles.whiteSpaceAfterDescription}></p>
             </div>
