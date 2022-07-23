efde92d35faf393901dd770dd806c96bb9ff1634
Wendy Teo <wendyteo.wy@gmail.com>
2022-07-23 15:23:04 +1000

    Add useState hook function to Icon

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,15 +4,19 @@ import styles from './Footer.module.css';
 
 function Footer() {
     return (
-        <div className={styles.footerStyling}>
+        <footer className={styles.footerStyling}>
             <p id={styles.contactLine}>Say hello!</p>
             <p>↓</p>
             <p id={styles.email}>wendyteo.wy@gmail.com</p>
             <p id={styles.phone}>(+61) 458 369 510</p>
-            <p id={styles.github}>github.com/wteo/</p>
+            <p id={styles.github}>
+                <a href='https://github.com/wteo/' target='_blank' rel='noreferrer'>
+                    github.com/wteo/
+                </a>
+            </p>
             <p id={styles.copyright}>© Wendy Teo 2022 •  Designs by Yours Truly</p>
             <p id={styles.place}>Sydney, Australia</p>
-        </div>
+        </footer>
     );
 }
 
--- src/components/Tools/Tools.js
+++ src/components/Tools/Tools.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { useState } from 'react';
+
 import NavTitleStyle from '../../UI/Nav-Title-style';
 import Tool from './Tool';
 import ToolsContainer from './ToolsContainer';
@@ -38,41 +40,65 @@ import github from '../../icons/github.png';
 
 
 function Tools() {
+    
+    const [iconImage, setIconImage] = useState(true);
+
+    const clickHandler = () => {
+        setIconImage(!iconImage);
+        console.log(iconImage);
+    };
+
     return (
         <div className={styles.toolsStyling}>
             <NavTitleStyle navColor='main'>
                 Tools
             </NavTitleStyle>
             <p className={styles.toolDescription}>These are the tools / technologies I have used to create my projects.</p>
+            <button className={styles.changeButton} onClick={clickHandler}>{iconImage ? 'Icon  →  Text' : 'Text  →  Icon'}</button>
             <section className={styles.toolsListGrid}>
                 <ToolsContainer toolType='Languages'>
-                    <Tool imageLink={jsIcon} toolName='JavaScript'/>
-                    <Tool imageLink={es6} toolName='ES6'/>
-                    <Tool imageLink={css} toolName='CSS'/>
-                    <Tool imageLink={html} toolName='HTML'/>
+                    {iconImage && <Tool imageLink={jsIcon} toolName='JavaScript'/>}
+                    {!iconImage && <p className={styles.toolText}>JavaScript</p>}
+                    {iconImage && <Tool imageLink={es6} toolName='ES6'/>}
+                    {!iconImage && <p className={styles.toolText}>ES6</p>}
+                    {iconImage && <Tool imageLink={css} toolName='CSS'/>}
+                    {!iconImage && <p className={styles.toolText}>CSS</p>}
+                    {iconImage && <Tool imageLink={html} toolName='HTML'/>}
+                    {!iconImage && <p className={styles.toolText}>HTML</p>}
                 </ToolsContainer>
                 <ToolsContainer toolType='JavaScript UI Library'>
-                    <Tool imageLink={react} toolName='react'/>
+                    {iconImage && <Tool imageLink={react} toolName='react'/>}
+                    {!iconImage && <p className={styles.toolText}>React</p>}
                 </ToolsContainer>
                 <ToolsContainer toolType='Code Editor'>
-                    <Tool imageLink={vscode} toolName='vscode'/>
+                    {iconImage && <Tool imageLink={vscode} toolName='vscode'/>}
+                    {!iconImage && <p className={styles.toolText}>VSCode</p>}
                 </ToolsContainer>
                 <ToolsContainer toolType='Package Manager'>
-                    <Tool imageLink={npm} toolName='NPM'/>
+                    {iconImage && <Tool imageLink={npm} toolName='NPM'/>}
+                    {!iconImage && <p className={styles.toolText}>NPM</p>}
                 </ToolsContainer>
                 <ToolsContainer toolType='Back-end Runtime Platforms'>
-                    <Tool imageLink={nodeJS} toolName='nodeJS'/>
-                    <Tool imageLink={firebase} toolName='Firebase'/>
+                    {iconImage && <Tool imageLink={nodeJS} toolName='nodeJS'/>}
+                    {!iconImage && <p className={styles.toolText}>NodeJS</p>}
+                    {iconImage && <Tool imageLink={firebase} toolName='Firebase'/>}
+                    {!iconImage && <p className={styles.toolText}>Firebase</p>}
                 </ToolsContainer>
                 <ToolsContainer toolType='JavaScript Frameworks / Modules'>
-                    <Tool imageLink={express} toolName='expressJS'/>
-                    <Tool imageLink={mocha} toolName='mochaJS'/>
-                    <Tool imageLink={chai} toolName='chaiJS'/>
-                    <Tool imageLink={nodemon} toolName='nodemon'/>
+                    {iconImage && <Tool imageLink={express} toolName='expressJS'/>}
+                    {!iconImage && <p className={styles.toolText}>Express</p>}
+                    {iconImage && <Tool imageLink={mocha} toolName='mochaJS'/>}
+                    {!iconImage && <p className={styles.toolText}>Mocha</p>}
+                    {iconImage && <Tool imageLink={chai} toolName='chaiJS'/>}
+                    {!iconImage && <p className={styles.toolText}>Chai</p>}
+                    {iconImage && <Tool imageLink={nodemon} toolName='nodemon'/>}
+                    {!iconImage && <p className={styles.toolText}>Nodemon</p>}
                 </ToolsContainer>
                 <ToolsContainer toolType='Deployment'>
-                    <Tool imageLink={netlify} toolName='netlify'/>
-                    <Tool imageLink={github} toolName='GitHub'/>
+                    {iconImage && <Tool imageLink={netlify} toolName='netlify'/>}
+                    {!iconImage && <p className={styles.toolText}>Netlify</p>}
+                    {iconImage && <Tool imageLink={github} toolName='GitHub'/>}
+                    {!iconImage && <p className={styles.toolText}>GitHub</p>}
                 </ToolsContainer>
             </section>
         </div>
