11fa3112b9619c1d97a115caef304f4033c674b4
Wendy Teo <wendyteo.wy@gmail.com>
2022-07-23 14:08:39 +1000

    Changed job descriptions from Grid to order listing

--- src/components/Experience/JobSummary/JobDescription.js
+++ src/components/Experience/JobSummary/JobDescription.js
@@ -4,23 +4,17 @@ import styles from './JobDescription.module.css';
 
 function JobDescription() {
     return (
-        <div className={styles.jobDescriptionStyle}>
-            <div className={styles.descriptionContainer}>
-                <p className={styles.dotPoint}>*</p>
-                <p className={styles.descriptionPar}>Involves in key projects to automate parts of Accounts Receivables (AR) procedure, testing system and providing feedback.</p>
-                <p className={styles.whiteSpaceAfterDescription}></p>
-            </div>
-            <div className={styles.descriptionContainer}>
-                <p className={styles.dotPoint}>*</p>
-                <p className={styles.descriptionPar}>Maintains day-to-day communication with clients via email/phone concerning aged/outstanding debts, liaising with high-profile personas, including CFOs, Company Secretaries, Senior Accountants, etc.</p>
-                <p className={styles.whiteSpaceAfterDescription}></p>
-            </div>
-            <div className={styles.descriptionContainer}>
-                <p className={styles.dotPoint}>*</p>
-                <p className={styles.descriptionPar}>Where necessary, contacts clients for payment and arranges/negotiates payment schedules.</p>
-                <p className={styles.whiteSpaceAfterDescription}></p>
-            </div>
-        </div>
+        <ul className={styles.jobDescriptionStyle}>
+            <li className={styles.task}>
+                Involves in key projects to automate parts of Accounts Receivables (AR) procedure, testing system and providing feedback.
+            </li>
+            <li className={styles.task}>
+                Maintains day-to-day communication with clients via email/phone concerning aged/outstanding debts, liaising with high-profile personas, including CFOs, Company Secretaries, Senior Accountants, etc.
+            </li>
+            <li className={styles.task}>
+                Where necessary, contacts clients for payment and arranges/negotiates payment schedules.
+            </li>
+        </ul>
     );
 }
 
